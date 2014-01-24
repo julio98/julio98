3f1ac40ef00af27a4947747b67573b9a95f63257
julio98 <julioe.r.ramos@gmail.com>
2014-01-23 23:26:46 -0500

    Update a.erl

--- a.erl
+++ a.erl
@@ -1,1 +1,12 @@
-<html>
+-<html>
++<!DOCTIPE html>
++<html lang="es">
++<head>
++    <meta charset="utf-8" />
++    <meta description="proyecto de diseño web de julio ramos ramos" />
++    <title>Proyecto de diseño web</title>
++</head>
++<body>
++
++</body>
++</html>
